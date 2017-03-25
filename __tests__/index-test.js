ca50b3e39d6f1d48696bf2ef3ffc30288a00864a
Dmitriy Bataev <bataevdmitriy@gmail.com>
2017-03-25 17:55:46 +0300

    Update tests

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -1,14 +1,55 @@
 import genDiff from '../src/'; 
 
-const out1 = `{
+describe('Simple configurations', () => {
+  const beforeJSON = '__tests__/__fixtures__/json-before.json';
+  const afterJSON = '__tests__/__fixtures__/json-after.json';
+  const beforeYAML = '__tests__/__fixtures__/yaml-before.yml';
+  const afterYAML = '__tests__/__fixtures__/yaml-after.yml';
+  const beforeINI = '__tests__/__fixtures__/ini-before.ini';
+  const afterINI = '__tests__/__fixtures__/ini-after.ini';
+
+  const out1 = `{
 	  host: hexlet.io
 	+ timeout: 20
 	- timeout: 50
 	- proxy: 123.234.53.22
-	+ verbose: true
-}`;
+	+ verbose: true\n}`;
 
-const out2 = `{
+  const out2 = `Property 'timeout' was updated. From '50' to '20'
+Property 'proxy' was removed
+Property 'verbose' was added with value: 'true'`;
+
+
+  it('compare two JSON files', () => {
+    const current = genDiff(beforeJSON, afterJSON);
+    expect(current).toBe(out1);
+  });
+
+  it('compare two YAML files', () => {
+    const current = genDiff(beforeYAML, afterYAML);
+    expect(current).toBe(out1);
+  });
+
+  it('compare two INI files', () => {
+    const current = genDiff(beforeINI, afterINI);
+    expect(current).toBe(out1);
+  });
+
+  it('compare two JSON files (plain format)', () => {
+    const current = genDiff(beforeJSON, afterJSON, { format: 'plain' });
+    expect(current).toBe(out2);
+  });
+});
+
+describe('Complex configurations', () => {
+  const beforeJSON = '__tests__/__fixtures__/extended-json-before.json';
+  const afterJSON = '__tests__/__fixtures__/extended-json-after.json';
+  const beforeYAML = '__tests__/__fixtures__/extended-yaml-before.yml';
+  const afterYAML = '__tests__/__fixtures__/extended-yaml-after.yml';
+  const beforeINI = '__tests__/__fixtures__/extended-ini-before.ini';
+  const afterINI = '__tests__/__fixtures__/extended-ini-after.ini';
+
+  const out1 = `{
 	  common: {
 		  setting1: Value 1
 		- setting2: 200
@@ -31,14 +72,9 @@ const out2 = `{
 	}
 	+ group3: {
 		  fee: 100500
-	}
-}`;
+	}\n}`;
 
-const out3 = `Property 'timeout' was updated. From '50' to '20'
-Property 'proxy' was removed
-Property 'verbose' was added with value: 'true'`;
-
-const out4 = `Property 'common.setting2' was removed
+  const out2 = `Property 'common.setting2' was removed
 Property 'common.setting6' was removed
 Property 'common.setting4' was added with value: 'blah blah'
 Property 'common.setting5' was added with complex value
@@ -46,59 +82,23 @@ Property 'group1.baz' was updated. From 'bas' to 'bars'
 Property 'group2' was removed
 Property 'group3' was added with complex value`;
 
-test('compare two JSON files', () => {
-  const path1 = '__tests__/__fixtures__/json-before.json';
-  const path2 = '__tests__/__fixtures__/json-after.json';
-  const current = genDiff(path1, path2);
-  expect(current).toBe(out1);
-});
+  it('compare two extended JSON files', () => {
+    const current = genDiff(beforeJSON, afterJSON);
+    expect(current).toBe(out1);
+  });
 
-test('compare two YAML files', () => {
-  const path1 = '__tests__/__fixtures__/yaml-before.yml';
-  const path2 = '__tests__/__fixtures__/yaml-after.yml';
-  const current = genDiff(path1, path2);
-  expect(current).toBe(out1);
-});
+  it('compare two extended YAML files', () => {
+    const current = genDiff(beforeYAML, afterYAML);
+    expect(current).toBe(out1);
+  });
 
-test('compare two INI files', () => {
-  const path1 = '__tests__/__fixtures__/ini-before.ini';
-  const path2 = '__tests__/__fixtures__/ini-after.ini';
-  const current = genDiff(path1, path2);
-  expect(current).toBe(out1);
-});
+  it('compare two extended INI files', () => {
+    const current = genDiff(beforeINI, afterINI);
+    expect(current).toBe(out1);
+  });
 
-test('compare two extended JSON files', () => {
-  const path1 = '__tests__/__fixtures__/extended-json-before.json';
-  const path2 = '__tests__/__fixtures__/extended-json-after.json';
-  const current = genDiff(path1, path2);
-  expect(current).toBe(out2);
-});
-
-test('compare two extended YAML files', () => {
-  const path1 = '__tests__/__fixtures__/extended-yaml-before.yml';
-  const path2 = '__tests__/__fixtures__/extended-yaml-after.yml';
-  const current = genDiff(path1, path2);
-  expect(current).toBe(out2);
-});
-
-test('compare two extended INI files', () => {
-  const path1 = '__tests__/__fixtures__/extended-ini-before.ini';
-  const path2 = '__tests__/__fixtures__/extended-ini-after.ini';
-  const current = genDiff(path1, path2);
-  expect(current).toBe(out2);
-});
-
-test('compare two JSON files (plain format)', () => {
-  const path1 = '__tests__/__fixtures__/json-before.json';
-  const path2 = '__tests__/__fixtures__/json-after.json';
-  const current = genDiff(path1, path2, { format: 'plain' });
-  expect(current).toBe(out3);
-});
-
-
-test('compare two extended JSON files (plain format)', () => {
-  const path1 = '__tests__/__fixtures__/extended-json-before.json';
-  const path2 = '__tests__/__fixtures__/extended-json-after.json';
-  const current = genDiff(path1, path2, { format: 'plain' });
-  expect(current).toBe(out4);
+  it('compare two extended JSON files (plain format)', () => {
+    const current = genDiff(beforeJSON, afterJSON, { format: 'plain' });
+    expect(current).toBe(out2);
+  });
 });
